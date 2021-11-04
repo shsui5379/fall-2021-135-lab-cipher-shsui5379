1066ebb33c5291af8a7d1561acd7059e349a71d7
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2021-11-04 01:00:18 -0400

    add source link

--- solve.cpp
+++ solve.cpp
@@ -16,6 +16,7 @@
  * */
 std::string solve(std::string encrypted_string)
 {
+    //http://cs.wellesley.edu/~fturbak/codman/letterfreq.html
     const std::vector<double> TARGET_FREQUENCY = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, .07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074};
 
     double shortestDistance = calculateDistance(generateVectorFromFrequency(encrypted_string), TARGET_FREQUENCY);
